df9067ae394db4a6624d20484a5de23eedfadae5
Ronny Perez <ronnypdev@gmail.com>
2020-09-03 16:19:44 -0400

    Implemented js functionality and fix js bugs

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
+// sets a starting point for array index
 let notificationNumber = 0;
+
+// Selects the notification-container from the DOM
 const notificationContainer = document.querySelector('#notify');
+
+// Selects form element from the DOM
 const form = document.querySelector('.form');
 
+/**
+ * Notifications {Array}
+ * Which holds the values for when the user submits the form
+ */
 const notifications = [
   {
     message:
@@ -14,21 +23,49 @@ const notifications = [
   },
 ];
 
-// Selects a random notification
+/**
+ * Selects a random notification from the {Array}
+ * also uses the updateHTMl() to update the new
+ * HTML injected into the DOM
+ */
 const random = () => {
   notificationNumber = Math.floor(Math.random() * notifications.length);
   updateHTMl();
 };
 
+/**
+ * This function loops through the notifications {Array}
+ * and it updates the notification-container with new the HTML
+ *
+ */
 const updateHTMl = () => {
   for (let i = 0; i < notifications.length; i++) {
     notificationContainer.innerHTML = notifications[notificationNumber].message;
+    notificationContainer.style.display = 'block';
   }
 };
 
+/**
+ * Attached a submit event to the form
+ * Also implemented the timeout for the 2 seconds delay
+ */
 form.addEventListener('submit', function (eve) {
+  eve.stopImmediatePropagation();
   eve.preventDefault();
   setTimeout(() => {
     random();
-  }, 5000);
+  }, 2000);
+});
+
+/**
+ * Click event for notifications to close
+ * once they are displayed
+ */
+notificationContainer.addEventListener('click', function (event) {
+  // If anything other than the close button was pressed, do nothing
+  if (!event.target.matches('.delete')) return;
+
+  if (event.target.matches('.delete')) {
+    notificationContainer.style.display = 'none';
+  }
 });
